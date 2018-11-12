9a7f77fec0d49d0590546f8389215a1f1769a2f6
Schmagel, Daniel <daniel.schmagel@trojans.dsu.edu>
2018-11-12 14:03:48 -0600

    Version 0.0.2

--- HockeySim/HockeySim/DefaultForm.Designer.cs
+++ HockeySim/HockeySim/DefaultForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HockeySim
 {
-    partial class Form1
+    partial class DefaultForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // DefaultForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1269, 917);
+            this.Name = "DefaultForm";
+            this.Text = "Hockey Sim";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
